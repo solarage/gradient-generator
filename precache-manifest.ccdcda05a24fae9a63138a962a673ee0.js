8276093b7264148bc849b9908865bb58951952d0
Alex <35805922+solarage@users.noreply.github.com>
2018-10-08 11:16:08 +0300

    build 01

---
+++ precache-manifest.ccdcda05a24fae9a63138a962a673ee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12baa244b302ec93e60a",
+    "url": "/gradient-generator/static/js/runtime~main.12baa244.js"
+  },
+  {
+    "revision": "5f990568e3fb08fba3ca",
+    "url": "/gradient-generator/static/js/main.5f990568.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/gradient-generator/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "5f990568e3fb08fba3ca",
+    "url": "/gradient-generator/static/css/main.1c165728.chunk.css"
+  },
+  {
+    "revision": "674280f674a4bf18373816b742173e1b",
+    "url": "/gradient-generator/index.html"
+  }
+];
